1275cbecd93c6ed2bf1ecac865ea7832f203ff06
RAGHAV GARG <raghavgarg.hcst.cs20@sgei.org>
2023-03-16 18:46:37 +0530

    login page changes

---
+++ main.js
@@ -0,0 +1,8 @@
+const button=document.getElementById('loginBtn');
+console.log(button);
+function onLoginUser(){
+    const username=document.getElementById('username').value;
+    const password=document.getElementById('pass').value;
+    console.log(username+" "+password);
+    console.log("hi");
+}
